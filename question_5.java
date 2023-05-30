43b74c8f091bfc7b1a0d7cba7049574524823036
Rajat Singh <singhr3234@gmail.com>
2023-05-30 21:14:43 +0530

    Question5 is added

---
+++ question_5.java
@@ -0,0 +1,12 @@
+public class question_5{
+    public static void main(String[] args) {
+        int[] arr={1,1,3,4,2,3,5,7,0};
+        for(int i=1;i<(arr.length-1);i++){
+            if(arr[i-1]<arr[i] && (arr[i]>arr[i+1])){
+                System.out.println("The first peak element is "+ arr[i]);
+                break;
+            }
+        }
+    }
+
+}
