db5d34bd42bf118617541e86c81e75d99f72d02a
Rajat Singh <singhr3234@gmail.com>
2023-05-30 20:50:09 +0530

    question2 is added

---
+++ Question_2.java
@@ -0,0 +1,14 @@
+public class Question_2 {
+    public static void main(String[] args) {
+        String [] arr_1={"ab","bc","cd","de","ef","fg","gh"};
+        int i=0;
+        while(i<arr_1.length){
+            if(i%2==1){
+                System.out.println(arr_1[i]);
+            }
+            i++;
+        }
+        
+    }
+    
+}
